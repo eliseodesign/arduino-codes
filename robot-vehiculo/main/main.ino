c64e26a102f2b8e9e015044cd9e0e3ac0eca31dc
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-06-21 14:24:23 -0600

    [:recycle: refactor] ino

--- robot-vehiculo/main/main.ino
+++ robot-vehiculo/main/main.ino
@@ -1,35 +1,47 @@
-#include <SoftwareSerial.h>  // Libreria para comunicacion serial
-#include <Servo.h>           // Libreria para servomotor
-#include "src/utils.h"           // Libreria para servomotor
-#include "src/move.h"           // Libreria para servomotor
+#include <SoftwareSerial.h>
+#include <Servo.h>
 
-SoftwareSerial bluetooth(9, 10);  //(Tx(9),Rx(10)) para comunicacion serial
-Servo left;                        // Nombramos al servomotor izquierdo como "left"
-Servo right;                       // Nombramos al servomotor derecho como "right"
-int grados_left = 0;                // Iniciamos en cero los grados del servomotor izquierdo
-int grados_right = 0;               // Iniciamos en cero los grados del servomotor derecho
+SoftwareSerial bluetooth(9, 10);
+Servo left;
+Servo right;
 
 void setup() {
-  Serial.begin(9600);           // Habilitamos comunicacion serial
-  bluetooth.begin(9600);        // Inicializamos la comunicacion con el HC-05
-  left.attach(12);              // Pin de control para el servomotor izquierdo
-  right.attach(11);             // Pin de control para el servomotor derecho
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  left.attach(12);
+  right.attach(11);
 }
 
 void loop() {
-  if (bluetooth.available()) {  // Si hay datos disponibles en el Bluetooth
-    String input = bluetooth.readString();  // Lee el string enviado por Bluetooth
-   
-    String comando = ObtenerMovimiento(input);
-    int separador = comando.indexOf(' ');
-    if (separador != -1) {
-      grados_left = extraerGrados( comando.substring(0, separador) );    // Obtiene la parte del comando para left
-      grados_left = extraerGrados( comando.substring(separador + 1) );  // Obtiene la parte del comando para right
+  if (bluetooth.available()) {
+    char input = bluetooth.read();
 
-      // Mueve los servomotores a las posiciones indicadas por grados_left y grados_right
-      left.write(grados_left);
-      right.write(grados_right);
+    int grados_left = 0;
+    int grados_right = 0;
+
+    if (input == 'g') {
+      grados_left = 40;
+      grados_right = 150;
+    } 
+    if (input == 'b') {
+      grados_left = 150;
+      grados_right = 50;
+    }
+    if (input == 'l') {
+      grados_left = 50;
+      grados_right = 50;
+    }
+    if (input == 'r') {
+      grados_left = 150;
+      grados_right = 150;
+    }
+    if (input == 's') {
+      grados_left = 90;
+      grados_right = 90;
     }
     
+    // Mueve los servomotores a las posiciones indicadas por grados_left y grados_right
+    left.write(grados_left);
+    right.write(grados_right);
   }
 }
