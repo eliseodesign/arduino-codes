e12b6c7e34f3d61107d977a033c4df632584ea4b
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2023-07-03 14:28:32 -0600

    [:sparkles: add] add counter

--- robot-vehiculo/main/main.ino
+++ robot-vehiculo/main/main.ino
@@ -5,6 +5,9 @@ SoftwareSerial bluetooth(9, 10);
 Servo left;
 Servo right;
 
+bool stopServo = false; // Variable para controlar el estado de detención del servomotor
+int servoStoppedCount = 0; // Contador de veces que se ha presionado '3'
+
 void setup() {
   Serial.begin(9600);
   bluetooth.begin(9600);
@@ -15,27 +18,40 @@ void setup() {
 void loop() {
   if (bluetooth.available()) {
     char input = bluetooth.read();
+    Serial.print(input);
 
     int grados_left = 0;
     int grados_right = 0;
 
-    if (input == 'g') {
-      grados_left = 40;
-      grados_right = 150;
+    if (input == '1') {
+      grados_left = 40;   //izquierdo
+      grados_right = 150; //derecho
     } 
-    if (input == 'b') {
-      grados_left = 150;
-      grados_right = 50;
+    if (input == '2') {
+      grados_left = 150; //izquierdo
+      grados_right = 50; //derecho
     }
-    if (input == 'l') {
-      grados_left = 50;
-      grados_right = 50;
+    if (input == '4') {
+      grados_left = 50; //izquierdo
+      grados_right = 50;//derecho
     }
-    if (input == 'r') {
-      grados_left = 150;
-      grados_right = 150;
+    if (input == '3') {
+      servoStoppedCount++;
+      
+      if (servoStoppedCount == 3) {
+        stopServo = !stopServo; // Cambiar el estado de detención del servomotor
+        servoStoppedCount = 0; // Reiniciar el contador
+      }
+      
+      if (stopServo) {
+        grados_left = 90; // Detener el servomotor en una posición neutral
+        grados_right = 90;
+      } else {
+        grados_left = 150; //izquierdo
+        grados_right = 150; //derecho
+      }
     }
-    if (input == 's') {
+    if (input == '5') {
       grados_left = 90;
       grados_right = 90;
     }
