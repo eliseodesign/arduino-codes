c529c74effb71132b8466a70fa9b12b5bdf05e07
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-24 18:38:13 -0600

    chore(robot-vehiculo-HC-SR04): :tada: esquivar objetos

--- robot-vehiculo-HC-SR04/main/main.ino
+++ robot-vehiculo-HC-SR04/main/main.ino
@@ -1,11 +1,19 @@
-int trigPin = 12;
-int echoPin = 11;
+#include <Servo.h>
+
+int trigPin = 8;
+int echoPin = 9;
 int ledPin = 13;
+int servoPin1 = 7; // Pin del primer servo
+int servoPin2 = 6; // Pin del segundo servo
+Servo myservo1;    // Objeto del primer servo
+Servo myservo2;    // Objeto del segundo servo
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(ledPin, OUTPUT);
+  myservo1.attach(servoPin1); // Inicializar el primer servo en el pin especificado
+  myservo2.attach(servoPin2); // Inicializar el segundo servo en el pin especificado
   Serial.begin(9600);
 }
 
@@ -13,14 +21,55 @@ void loop() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   long duration = pulseIn(echoPin, HIGH);
   int distance = duration * 0.034 / 2;
-  
-  if (distance < 100) {
+
+  if (distance < 10) { // Si hay un objeto cercano
     Serial.println(distance);
     digitalWrite(ledPin, HIGH);
-    delay(50);
-  }
+
+    // Detener el movimiento
+    myservo1.write(90); // Ángulo de 90 grados (posición central)
+    myservo2.write(90); // Ángulo de 90 grados (posición central)
+
+    delay(500);
+
+    myservo1.write(0); //Retroceso
+    myservo2.write(189); // Retroceso
+    delay(2000);
+
+    bool espacio = false;
+
+    while (!espacio) { // se puede avanzar
+      Serial.println("While");
+      // girar hasta estar despejado el camino
+      myservo1.write(180);
+      myservo2.write(180);
+      delay(500);
+
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+
+      long duration = pulseIn(echoPin, HIGH);
+      int distance = duration * 0.034 / 2;
+
+      if (distance > 10) {
+        espacio = true; // habilitar para avanzar
+      }
+    }
+
+    // Detener el movimiento después de encontrar espacio
+    myservo1.write(90); // Ángulo de 90 grados (posición central)
+    myservo2.write(90); // Ángulo de 90 grados (posición central)
+    delay(1000);
+
+  } else { // Si no hay un objeto cercano
     digitalWrite(ledPin, LOW);
+
+    // Avanzar recto
+    myservo1.write(180); // Ángulo de 180 grados (máxima velocidad hacia adelante)
+    myservo2.write(0);   // Ángulo de 0 grados (máxima velocidad hacia adelante)
+  }
 }
