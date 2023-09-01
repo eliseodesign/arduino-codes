bc65e8068b355637872e306361c42d62a7b616ef
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-31 20:24:17 -0600

    fix: :sparkles: esquivar

--- robot-vehiculo-HC-SR04/main/main.ino
+++ robot-vehiculo-HC-SR04/main/main.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
-int trigPin = 8;
-int echoPin = 9;
+int trigPin = 9;
+int echoPin = 10;
 int ledPin = 13;
 int servoPin1 = 7; // Pin del primer servo
 int servoPin2 = 6; // Pin del segundo servo
@@ -17,36 +17,37 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
+int getDistance() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
   long duration = pulseIn(echoPin, HIGH);
   int distance = duration * 0.034 / 2;
+  return distance;
+}
 
-  if (distance < 10) { // Si hay un objeto cercano
-    Serial.println(distance);
-    digitalWrite(ledPin, HIGH);
+void loop() {
+  delay(100);
+  int distance = getDistance();
 
+  Serial.println(distance);
+  if (distance < 15) { // Si hay un objeto cercano
     // Detener el movimiento
     myservo1.write(90); // Ángulo de 90 grados (posición central)
     myservo2.write(90); // Ángulo de 90 grados (posición central)
 
     delay(500);
 
-    myservo1.write(0); //Retroceso
-    myservo2.write(189); // Retroceso
-    delay(2000);
 
     bool espacio = false;
 
     while (!espacio) { // se puede avanzar
       Serial.println("While");
       // girar hasta estar despejado el camino
-      myservo1.write(180);
-      myservo2.write(180);
-      delay(500);
+      myservo1.write(30);
+      myservo2.write(30);
+      delay(700);
 
       digitalWrite(trigPin, HIGH);
       delayMicroseconds(10);
@@ -64,12 +65,12 @@ void loop() {
     myservo1.write(90); // Ángulo de 90 grados (posición central)
     myservo2.write(90); // Ángulo de 90 grados (posición central)
     delay(1000);
+    return;
 
-  } else { // Si no hay un objeto cercano
-    digitalWrite(ledPin, LOW);
+  } 
+  digitalWrite(ledPin, LOW);
 
-    // Avanzar recto
-    myservo1.write(180); // Ángulo de 180 grados (máxima velocidad hacia adelante)
-    myservo2.write(0);   // Ángulo de 0 grados (máxima velocidad hacia adelante)
-  }
+  // Avanzar recto
+  myservo1.write(150); // Ángulo de 180 grados (máxima velocidad hacia adelante)
+  myservo2.write(30);   // Ángulo de 0 grados (máxima velocidad hacia adelante)
 }
